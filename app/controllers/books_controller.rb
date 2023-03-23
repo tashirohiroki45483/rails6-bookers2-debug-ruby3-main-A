1a69e20ff846ce1de033989641e1b41de48e3884
EC2 Default User <ec2-user@ip-172-31-13-161.ap-northeast-1.compute.internal>
2023-03-23 07:07:34 +0000

    iine comment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book2 =  Book.new
+    @book2 = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
   devise_for :users
   resources :users, only: [:index,:show,:edit,:update]
-  
-  
+
+
 end
