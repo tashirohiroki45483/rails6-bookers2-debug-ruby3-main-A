75340712a15552997832288682d05055976731e3
EC2 Default User <ec2-user@ip-172-31-13-161.ap-northeast-1.compute.internal>
2023-03-24 07:17:15 +0000

    follow

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -10,7 +10,7 @@ class BookCommentsController < ApplicationController
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  resources :users, only: [:index,:show,:edit,:update]
-
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
