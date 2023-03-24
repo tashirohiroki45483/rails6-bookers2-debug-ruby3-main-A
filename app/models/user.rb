0af194a938958e84ad30ff00800fcb912d82beb0
EC2 Default User <ec2-user@ip-172-31-13-161.ap-northeast-1.compute.internal>
2023-03-24 12:15:25 +0000

    search

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,20 @@ def following?(user)
   followings.include?(user)
 end
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   has_one_attached :profile_image
 
   validates :name,
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
+  get "search" => "searches#search"
+  
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
