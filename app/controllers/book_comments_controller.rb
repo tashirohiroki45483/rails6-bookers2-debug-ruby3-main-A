de69586a80fd0cb0341957705f3ea5de1a76911b
EC2 Default User <ec2-user@ip-172-31-13-161.ap-northeast-1.compute.internal>
2023-04-03 07:37:30 +0000

    コメント非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,21 @@
 class BookCommentsController < ApplicationController
 
-  def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
-  end
+def create
+  @book = Book.find(params[:book_id])
+  @book_comment = current_user.book_comments.new(book_comment_params)
+  @book_comment.book_id = @book.id
+  @book_comment.save
+end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    if book_comment
+      book_comment.destroy
+      flash.now[:alert] = '投稿を削除しました'
+    else
+      flash.now[:alert] = '投稿が見つかりませんでした'
+    end
+    @book = Book.find(params[:book_id])
   end
 
   private
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,24 @@ class BooksController < ApplicationController
     @book_comment = BookComment.new
     @book_detail = Book.find(params[:id])
     current_user.view_counts.create(book_id: @book_detail.id)
+    @user = @book.user
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def index
